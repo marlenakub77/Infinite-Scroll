458f7e32c4ad6c4b6f81ed9a87719f236b045f96
marlenakub <marlena.kubicka@wp.pl>
2022-02-17 19:56:27 +0100

    Infinite Scroll_zakonczone

---
+++ js/scrypt.js
@@ -0,0 +1,89 @@
+    let endOfThePage = 0;
+
+    let preloading = false;
+
+    const showPreloader = () => {
+
+        let preloader = document.getElementById('preloader');
+        console.log('showPreloader()');
+        preloader.style.display = 'block';
+        preloading = true;
+    }
+
+    const hidePreloader = () => {
+
+        let preloader = document.getElementById('preloader');
+        console.log('hidePreloader()');
+        preloader.style.display = 'none';
+        preloading = false;
+    }
+
+    const getData = () => {
+
+        if (!preloading) {
+            showPreloader();
+        }
+        
+        fetch('https://akademia108.pl/api/ajax/get-users.php')
+            .then(res => res.json())
+            .then(data => {
+
+                let body = document.body;
+                let hr = document.createElement('hr');
+                body.appendChild(hr);
+
+                for (let user of data) {
+                    let pId = document.createElement('p');
+                    let pName = document.createElement('p');
+                    let pWebsite = document.createElement('p');
+
+                    pId.innerText = `User ID: ${user.id}`;
+                    pName.innerText = `User ID: ${user.name}`;
+                    pWebsite.innerHTML = `User URL: ${user.website}<br />==============`;
+
+                    body.appendChild(pId);
+                    body.appendChild(pName);
+                    body.appendChild(pWebsite);
+                }
+
+                hidePreloader();
+
+                console.log(data);
+            })
+
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    const scrollToEndOfPage = () => {
+
+        let d = document.documentElement;
+
+        // wysokosc strony
+        let scrollHeight = d.scrollHeight;
+
+        //liczba pixeli scrollowane od góry
+        let scrollTop = d.scrollTop;
+
+        //wewnętrzna wysokość (pix) przegladarki
+        let clientHeight = d.clientHeight;
+
+        let sumScrollTopClientHeight = Math.ceil(scrollTop + clientHeight);
+
+        console.log('scrollHeight: ${scrollHeight}');
+        console.log('sumScrollTopClientHeight: ${sumScrollTopClientHeight}');
+        console.log('scrollTop: ${scrollTop}');
+        console.log('clientHeight: ${clientHeight}');
+        console.log('==================');
+
+        if(sumScrollTopClientHeight >= scrollHeight) {
+            
+            endOfThePage += 1;
+            console.log(`Scrolled to the end of page: ${endOfThePage}`);
+
+            getData();
+        }
+    }
+
+    window.addEventListener('scroll', scrollToEndOfPage);
